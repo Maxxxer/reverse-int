b3bdbc38d1b4b6959a7e51c5bfffbc5cfcf28120
Maksim Palanets <Palanets_M@iba>
2022-06-26 14:45:52 +0300

    init: add script finish the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(number) {
+    number = Math.abs(number);
+    number = number.toString().split('');
+    let opposite = [];
+    for (let item of number) {
+        opposite.unshift(item);
+    }
+    return +opposite.join('');
 }
+
